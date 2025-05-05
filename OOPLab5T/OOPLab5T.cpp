bcc5e1b15c2d8213241d77b7aa3a731f23fa5a7d
dmytroT <tymoshchuk.dmytro@chnu.edu.ua>
2025-05-05 17:00:24 +0300

    1.0

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,89 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+const double PI = 3.141592653589793;
+// Base class Triangle
+class Triangle {
+protected:
+    double a, b, c; // sides of the triangle
 
+public:
+    // Constructor
+    Triangle(double sideA, double sideB, double sideC) {
+        a = sideA;
+        b = sideB;
+        c = sideC;
+    }
 
-#include <iostream>
+    // Methods to set sides
+    void setSides(double sideA, double sideB, double sideC) {
+        a = sideA;
+        b = sideB;
+        c = sideC;
+    }
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
-int main()
-{
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+    // Method to calculate perimeter
+    double getPerimeter() const {
+        return a + b + c;
+    }
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+    // Method to calculate angles using the cosine rule
+    void getAngles(double& A, double& B, double& C) const {
+        A = acos((b * b + c * c - a * a) / (2 * b * c)) * 180.0 / PI;
+        B = acos((a * a + c * c - b * b) / (2 * a * c)) * 180.0 / PI;
+        C = 180.0 - A - B;
+    }
+};
 
+// Derived class RightAngled
+class RightAngled : public Triangle {
+private:
+    double area;
+
+public:
+    // Constructor
+    RightAngled(double sideA, double sideB, double sideC)
+        : Triangle(sideA, sideB, sideC) {
+        if (!isRightAngled()) {
+            cout << "Warning: The triangle is not right-angled!" << endl;
+        }
+    }
+
+    // Check for right-angled triangle
+    bool isRightAngled() const {
+        double sides[3] = { a, b, c };
+        sort(sides, sides + 3);
+        return fabs(sides[2] * sides[2] - (sides[0] * sides[0] + sides[1] * sides[1])) < 1e-6;
+    }
+
+    // Calculate area
+    double getArea() {
+        double sides[3] = { a, b, c };
+        sort(sides, sides + 3); // sides[2] is hypotenuse
+        area = 0.5 * sides[0] * sides[1];
+        return area;
+    }
+};
+
+// Main function
+int main() {
+    // Create a general triangle
+    Triangle t(3, 4, 5);
+    double angleA, angleB, angleC;
+    t.getAngles(angleA, angleB, angleC);
+
+    cout << "Triangle sides: 3, 4, 5" << endl;
+    cout << "Perimeter: " << t.getPerimeter() << endl;
+    cout << "Angles (in degrees): A = " << angleA
+        << ", B = " << angleB
+        << ", C = " << angleC << endl;
+
+    // Create a right-angled triangle
+    RightAngled rt(3, 4, 5);
+    cout << "\nRight-angled triangle check: "
+        << (rt.isRightAngled() ? "Yes" : "No") << endl;
+    cout << "Area: " << rt.getArea() << endl;
+
+    return 0;
 }
